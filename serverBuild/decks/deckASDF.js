e290330418597ffaddf23f624ad989e7f15d9bb2
Jonathan Hawley-Peters <jhawleypeters@gmail.com>
2019-02-19 14:42:55 +0800

    added server build files

---
+++ serverBuild/decks/deckASDF.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ASDFdeck = [
+    "Flying Kick Pin",
+    "Flying Kick",
+    "Zen State",
+    "Nailed!",
+    "Smash the Bridge",
+    "Luring Trap",
+    "Choke",
+    "Coil Up",
+    "Reposition",
+    "Backflip",
+    "Leg Grab",
+    "Pull By The Neck",
+    "Kip Up",
+    "Leaping Smash",
+    "1000 Cuts"
+];
+exports.ASDFdescription = "A silly deck, fun";
+exports.ASDFname = "A.S.D.F.";
--- serverBuild/decks/index.js
+++ serverBuild/decks/index.js
@@ -5,13 +5,15 @@ const deckGrapple_1 = require("./deckGrapple");
 const deckHighGround_1 = require("./deckHighGround");
 const deckGaldiator_1 = require("./deckGaldiator");
 const deckStone_1 = require("./deckStone");
+const deckASDF_1 = require("./deckASDF");
 const testDeck = ["ForcefulTest", "PredictTest", "GameEnder"];
 exports.decks = [
     { name: 'Grapple', deckList: deckGrapple_1.grappleDeck, description: "Excels at fighting while Grappled and Prone. Wants to play the card Neck Break (requires Grappled, Both Prone, and Balanced." },
     { name: 'High Ground', deckList: deckHighGround_1.highGroundDeck, description: "Wants to be standing while the opponent is prone. Tries very hard to avoid negative statuses" },
     { name: 'Gladiator', deckList: deckGaldiator_1.gladiatorDeck, description: "The deck wants to have anticipation, and the enemy be off balanced. It wants to be on it's feet, with the opponent moving or prone" },
     { name: 'Stone', deckList: deckStone_1.stoneDeck, description: "Focuses on block, choice, and momentum. Can do almost nothing if unbalanced." },
-    { name: 'test', deckList: testDeck, description: "Test deck, don't click this" }
+    { name: 'test', deckList: testDeck, description: "Test deck, don't click this" },
+    { name: deckASDF_1.ASDFname, deckList: deckASDF_1.ASDFdeck, description: deckASDF_1.ASDFdescription },
 ];
 exports.getDeckOptions = () => {
     return exports.decks.map((deck) => ({ name: deck.name, description: deck.description }));
