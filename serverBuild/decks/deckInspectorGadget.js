942a2f27e9c3ea91806ef4b79539916a6850d6f1
Jonathan Hawley-Peters <jhawleypeters@gmail.com>
2019-02-20 00:10:52 +0800

    Added Inspector Gadget Deck its OP six new cards: Smoke Bomb Drop Chandelier Insidious Plan Bullet Time Holographic Copies Drone Strike
    ...and that brings us to 100 cards! :D

---
+++ serverBuild/decks/deckInspectorGadget.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.inspectorGadgetDeck = [
+    "Smoke Bomb",
+    "Drop Chandelier",
+    "Insidious Plan",
+    "Bullet Time",
+    "Holographic Copies",
+    "Fancy Footwork",
+    "Femoral Strike",
+    "Drone Strike",
+    "Kip Up",
+    "Wind Up",
+    "Clever Reposition",
+    "Targeted Strike",
+    "Backflip",
+    "Luring Trap",
+    "Nailed!"
+];
+exports.inspectorGadgetDescription = "Focus oriented, this deck relies on classicly powerful states like Anticipation and ";
+exports.inspectorGadgetName = "Inspector Gadget";
