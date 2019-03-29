46bc889fc7b2154179030aff503cf1eda0db7ac2
Corey Wolff <jesterswilde@gmail.com>
2019-03-29 15:22:32 -0700

    First pass at hooking up heroku db

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,4 +1,5 @@
-import { createConnection, Repository } from "typeorm";
+import { createConnection, Repository, ConnectionOptions } from "typeorm";
+import * as urLReader from 'pg-connection-string';
 import { DBCard } from './entities/card';
 import { DBDeck } from "./entities/deck";
 import { DBUser } from "./entities/user";
@@ -7,21 +8,37 @@ export let cardRepo: Repository<DBCard>
 export let deckRepo: Repository<DBDeck>
 export let userRepo: Repository<DBUser>
 
-createConnection({
-    'type': 'postgres',
-    'host': 'localhost',
-    'username': 'postgres',
-    'password': '',
-    'database': 'fight',
-    'synchronize': true,
-    'entities': [
+let connectionObj: ConnectionOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '',
+    database: 'fight',
+    synchronize: true,
+    entities: [
         DBCard,
         DBDeck,
         DBUser
     ]
-}).then((connection)=>{
-    cardRepo = connection.getRepository(DBCard); 
-    deckRepo = connection.getRepository(DBDeck); 
-    userRepo = connection.getRepository(DBUser); 
-    console.log("conencted to db"); 
+}
+
+if (process.env.DATABASE_URL) {
+    const dbURL = urLReader.parse(process.env.DATABASE_URL);
+    connectionObj = {
+        ...connectionObj, ...{
+            username: dbURL.user,
+            password: dbURL.password,
+            host: dbURL.host,
+            port: dbURL.port,
+            extras: { ssl: true }
+        }
+    }
+}
+
+
+createConnection(connectionObj).then((connection) => {
+    cardRepo = connection.getRepository(DBCard);
+    deckRepo = connection.getRepository(DBDeck);
+    userRepo = connection.getRepository(DBUser);
+    console.log("conencted to db");
 })
--- serverBuild/db/index.js
+++ serverBuild/db/index.js
@@ -1,22 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
+const urLReader = require("pg-connection-string");
 const card_1 = require("./entities/card");
 const deck_1 = require("./entities/deck");
 const user_1 = require("./entities/user");
-typeorm_1.createConnection({
-    'type': 'postgres',
-    'host': 'localhost',
-    'username': 'postgres',
-    'password': '',
-    'database': 'fight',
-    'synchronize': true,
-    'entities': [
+let connectionObj = {
+    type: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '',
+    database: 'fight',
+    synchronize: true,
+    entities: [
         card_1.DBCard,
         deck_1.DBDeck,
         user_1.DBUser
     ]
-}).then((connection) => {
+};
+if (process.env.DATABASE_URL) {
+    const dbURL = urLReader.parse(process.env.DATABASE_URL);
+    connectionObj = Object.assign({}, connectionObj, {
+        username: dbURL.user,
+        password: dbURL.password,
+        host: dbURL.host,
+        port: dbURL.port,
+        extras: { ssl: true }
+    });
+}
+typeorm_1.createConnection(connectionObj).then((connection) => {
     exports.cardRepo = connection.getRepository(card_1.DBCard);
     exports.deckRepo = connection.getRepository(deck_1.DBDeck);
     exports.userRepo = connection.getRepository(user_1.DBUser);
