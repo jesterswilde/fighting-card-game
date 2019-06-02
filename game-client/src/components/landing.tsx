f74af7db77e707ef147a46aa09889f24f4d3fa58
Jonathan Hawley-Peters <jhawleypeters@gmail.com>
2019-06-01 20:47:30 -0700

    Landing / Style Viewer (#4)
    * Landing: remove login and signup, update wording
    
    * Style Viewer: update wording, add description
    
    * build files
    
    * put the B back in uilder

--- game-client/src/components/landing.tsx
+++ game-client/src/components/landing.tsx
@@ -5,28 +5,18 @@ export default () => {
     return <div class='landing'>
         <h2>Fighting Card Game <small>Without a name</small></h2>
         <div class='section'>
-
             <div>
                 <a class='link' onClick={() => to('/game')}>Play Game</a>
             </div>
             <div>
-                <a class='link' onClick={() => to('/decks')}>View Decks</a>
+                <a class='link' onClick={() => to('/decks')}>Starter Decks</a>
             </div>
             <div>
-                <a class='link' onClick={() => to('/styles')}>View Styles</a>
+                <a class='link' onClick={() => to('/styles')}>Fighting Styles</a>
             </div>
             <div>
-                <a class="link" onClick={() => to('/user/login')}>Login</a>
-            </div>
-            <div>
-                <a class="link" onClick={() => to('/user/create')}>Create Account</a>
-            </div>
-            <div>
-                <a class="link" onClick={() => to('/builder')}>Make a Deck</a>
+                <a class="link" onClick={() => to('/builder')}>Deck Builder</a>
             </div>
         </div>
-
-
     </div>
-
 }
