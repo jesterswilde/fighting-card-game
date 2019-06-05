95998ce5f537cb1d37e56abfd58ea5690bb59566
Corey Wolff <jesterswilde@gmail.com>
2019-06-04 22:47:57 -0700

    Styles now send proper payload to server when updating.

--- game-client/src/deckBuilder/util.ts
+++ game-client/src/deckBuilder/util.ts
@@ -5,11 +5,22 @@ import isEqual from 'lodash/isEqual'
 export const getUpdateDeckObj = (): { updateDeckObj: UpdateDeckObj, id: number } => {
     const updateDeckObj: UpdateDeckObj = {};
     const { deckEditor: { deck, uneditedDeck } } = store.getState();
-    if (!isEqual(deck.cards, uneditedDeck.cards)) {
-        updateDeckObj.cards = deck.cards;
-    }
-    if (!isEqual(deck.styles, uneditedDeck.styles)) {
+    const cardsChanged = !isEqual(deck.cards, uneditedDeck.cards)
+    const stylesChanged = !isEqual(deck.styles, uneditedDeck.styles)
+    if (stylesChanged) {
         updateDeckObj.styles = deck.styles;
+    }
+    if (stylesChanged || cardsChanged) {
+        updateDeckObj.cards = deck.cards.filter((cardName) => {
+            for (const style in deck.possibleCards) {
+                const cards = deck.possibleCards[style];
+                const hasCard = cards.some(({ name }) => name === cardName);
+                if (hasCard) {
+                    return true;
+                }
+            }
+            return false;
+        });
     }
     if (deck.name !== uneditedDeck.name) {
         updateDeckObj.name = deck.name;
