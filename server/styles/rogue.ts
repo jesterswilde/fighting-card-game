cfe24ac496857eb14cfb79b6bf642e01b5a97aee
Jonathan Hawley-Peters <jhawleypeters@gmail.com>
2019-06-15 21:17:07 -0700

    added Rogue style and cards

---
+++ server/styles/rogue.ts
@@ -0,0 +1,17 @@
+import { FightingStyle } from "./interfaces";
+
+export const rogueStyle: FightingStyle = {
+    name: "Rogue",
+    cards: [
+        'Apply Poison',
+        'Distract',
+        'Dodge',
+        'Engage',
+        'Flying Blade',
+        'Garrot',
+        'Roll Away',
+        'Shadow Play',
+        'Shiv',
+        'Stab',
+    ]
+}
